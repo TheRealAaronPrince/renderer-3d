768a73f077352ff6e1ff5852c9558d07b95f3a24
TheRealAaronPrince <36848021+TheRealAaronPrince@users.noreply.github.com>
2024-07-27 21:10:29 +0100

    added mouse control

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Windows;
 using System.Diagnostics;
 using System.Windows.Controls;
+using System.Runtime.InteropServices;
 using System.Windows.Media;
 using System.Windows.Input;
 using System.IO;
@@ -10,6 +11,9 @@ namespace renderer_3d
 {
 	class Program
 	{
+		static bool mouseHeld = true;
+		static float deltaX,deltaY;
+		static float sensitivity = 2f;
 		static int width = 1280;
 		static int height = 960;
 		static projection projection = new projection(width,height);
@@ -39,13 +43,29 @@ namespace renderer_3d
 			w.MinHeight = height;
 			w.MaxHeight = height;
 			w.KeyDown += new KeyEventHandler(w_KeyDown);
+			w.Title = "3d rendering";
 			w.Content = projection.vector.render.i;
 			w.Show();
+			setMousePos(new Point(width/2,height/2));
+			w.MouseMove += new MouseEventHandler(w_MouseMove);
 			project();
 			app.Run();
 		}
+		private static void setMousePos(Point position)
+		{
+			Point toWindow = w.PointToScreen(position);
+			SetCursorPos(Convert.ToInt32(toWindow.X),Convert.ToInt32(toWindow.Y));
+		}
+		[DllImport("User32.dll")]
+    	private static extern bool SetCursorPos(int X, int Y);
 		private static void project()
 		{
+			angleX = (angleX + 720)%360;
+			angleY = (angleY + 720)%360;
+			angleZ = (angleZ + 720)%360;
+			rotateX = (rotateX + 720)%360;
+			rotateY = (rotateY + 720)%360;
+			rotateZ = (rotateZ + 720)%360;
 			projection.rotX = angleX;
 			projection.rotY = angleY;
 			projection.rotZ = angleZ;
@@ -57,55 +77,33 @@ namespace renderer_3d
 			projection.angZ = rotateZ;
 			projection.drawObj(obj, faces);
 		}
+		private static void w_MouseMove(object sender, MouseEventArgs e)
+		{
+			if(mouseHeld)
+			{
+				deltaX = (float)(e.GetPosition(w).X - width/2);
+				deltaY = (float)(e.GetPosition(w).Y - height/2);
+				angleY -= deltaX*sensitivity*step;
+				angleX += deltaY*sensitivity*step;
+				setMousePos(new Point(width/2,height/2));
+				project();
+			}
+		}
 		private static void w_KeyDown(object sender, KeyEventArgs e)
 		{
-			angleX = (angleX + 720)%360;
-			angleY = (angleY + 720)%360;
-			angleZ = (angleZ + 720)%360;
-			rotateX = (rotateX + 720)%360;
-			rotateY = (rotateY + 720)%360;
-			rotateZ = (rotateZ + 720)%360;
 			switch (e.Key)
 			{
 				case Key.Up:
-					if(rotObj)
-					{
-						rotateX -= 2.5f;
-					}
-					else
-					{
-						angleX -= 2.5f;
-					}
+					rotateX -= 2.5f;
 					break;
 				case Key.Down:
-					if(rotObj)
-					{
-						rotateX += 2.5f;
-					}
-					else
-					{
-						angleX += 2.5f;
-					}
+					rotateX += 2.5f;
 					break;
 				case Key.Left:
-					if(rotObj)
-					{
-						rotateY += 2.5f;
-					}
-					else
-					{
-						angleY += 2.5f;
-					}
+					rotateY += 2.5f;
 					break;
 				case Key.Right:
-					if(rotObj)
-					{
-						rotateY -= 2.5f;
-					}
-					else
-					{
-						angleY -= 2.5f;
-					}
+					rotateY -= 2.5f;
 					break;
 				case Key.D:
 					posZ = (float)(posZ+(Math.Sin(angleY*(Math.PI/180))*step));
@@ -140,6 +138,20 @@ namespace renderer_3d
 					break;
 				case Key.PageDown:
 					step /=2;
+					break;
+				case Key.Escape:
+					mouseHeld = !mouseHeld;
+					break;
+				case Key.R:
+					angleX = 0.0f;
+					angleY = 0.0f;
+					angleZ = 0.0f;
+					rotateX = 0.0f;
+					rotateY = 0.0f;
+					rotateZ = 0.0f;
+					posX = 0.0f;
+					posY = 0.0f;
+					posZ = -0.75f;
 					break;
 			}
 			project();
